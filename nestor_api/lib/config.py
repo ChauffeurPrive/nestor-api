e0cd3fa6c33446dc7041ab294db8a4e33503e1a2
Maxime Jimenez <maxime.jimenez@kapten.com>
2020-06-26 17:00:32 +0200

    Add placeholder config lib to mock dependency

---
+++ nestor_api/lib/config.py
@@ -0,0 +1,9 @@
+"""Configuration library"""
+
+
+def get_app_config(app_name):
+    """Retrieves the configuration of an application"""
+
+    # Awaiting for implementation
+
+    return dict({"app_name": app_name})
