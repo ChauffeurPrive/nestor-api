3f0e57960a45729773ce554342414ea53ad28c83
Christophe Verclytte <1611574+chris-verclytte@users.noreply.github.com>
2020-06-29 07:30:09 +0200

    Factorize io mocks

--- tests/lib/test_git.py
+++ tests/lib/test_git.py
@@ -10,8 +10,8 @@ import nestor_api.lib.git as git
 
 @pytest.mark.usefixtures("config")
 # pylint: disable=no-self-use
+@patch('nestor_api.lib.git.io', autospec=True)
 class TestGitLibrary:
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_branch_existing(self, io_mock):
         io_mock.execute.side_effect = ["* feature/branch remotes/origin/feature/branch", ""]
 
@@ -28,7 +28,6 @@ class TestGitLibrary:
             )
         ])
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_branch_not_existing(self, io_mock):
         io_mock.execute.side_effect = ["", ""]
 
@@ -45,7 +44,6 @@ class TestGitLibrary:
             )
         ])
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     @patch('nestor_api.lib.git.update_pristine_repository', autospec=True)
     @patch('nestor_api.lib.git.config', autospec=True)
     def test_create_working_repository(self, config_mock, update_pristine_repository_mock, io_mock):
@@ -59,7 +57,6 @@ class TestGitLibrary:
         io_mock.create_temporary_copy.assert_called_once_with("/fixtures-nestor-pristine/my-app", "my-app")
         assert repository_dir == "/fixtures-nestor-work/my-app-11111111111111"
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_get_last_commit_hash(self, io_mock):
         io_mock.execute.return_value = "1ab2c3d"
 
@@ -71,7 +68,6 @@ class TestGitLibrary:
             "/path_to/a_git_repository"
         )
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_get_last_tag(self, io_mock):
         io_mock.execute.return_value = "1.0.0-sha-a2b3c4"
 
@@ -83,7 +79,6 @@ class TestGitLibrary:
             "/path_to/a_git_repository"
         )
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_get_remote_url_with_default_remote_name(self, io_mock):
         io_mock.execute.return_value = "git@github.com:org/repo.git"
 
@@ -95,7 +90,6 @@ class TestGitLibrary:
             "/path_to/a_git_repository",
         )
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_get_remote_url_with_remote_name(self, io_mock):
         io_mock.execute.return_value = "git@github.com:org/repo.git"
 
@@ -107,7 +101,6 @@ class TestGitLibrary:
             "/path_to/a_git_repository",
         )
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_push(self, io_mock):
         io_mock.execute.return_value = "git@github.com:org/repo.git"
 
@@ -118,7 +111,6 @@ class TestGitLibrary:
             "/path_to/a_git_repository",
         )
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     @patch('nestor_api.lib.git.config', autospec=True)
     def test_tag(self, config_mock, io_mock):
         config_mock.get_app_config.return_value = {"workflow": ["master", "staging", "production"]}
@@ -134,7 +126,6 @@ class TestGitLibrary:
         )
         assert tag == "1.0.0-sha-1ab2c3d"
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     @patch('nestor_api.lib.git.config', autospec=True)
     def test_tag_with_invalid_version(self, config_mock, io_mock):
         config_mock.get_app_config.return_value = {"workflow": ["master", "staging", "production"]}
@@ -145,9 +136,10 @@ class TestGitLibrary:
 
     @patch('nestor_api.lib.git.update_repository', autospec=True)
     @patch('nestor_api.lib.git.config', autospec=True)
-    def test_update_pristine_repository(self, config_mock, update_repository_mock):
+    def test_update_pristine_repository(self, config_mock, update_repository_mock, io_mock):
         config_mock.get_app_config.return_value = {"git": {"origin": "git@github.com:org/repo.git"}}
         update_repository_mock.return_value = ""
+        io_mock.get_pristine_path.return_value = "/fixtures-nestor-pristine/my-app"
 
         repository_dir = git.update_pristine_repository("my-app")
 
@@ -158,7 +150,6 @@ class TestGitLibrary:
         )
         assert repository_dir == "/fixtures-nestor-pristine/my-app"
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_update_repository_clone_if_not_existing_default_revision(self, io_mock):
         io_mock.exists.return_value = False
         io_mock.execute.side_effect = ["", ""]
@@ -171,7 +162,6 @@ class TestGitLibrary:
             call("git reset --hard origin/master", "/path_to/a_git_repository"),
         ])
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_update_repository_clone_if_not_existing_branch_revision(self, io_mock):
         io_mock.exists.return_value = False
         io_mock.execute.side_effect = ["", ""]
@@ -186,7 +176,6 @@ class TestGitLibrary:
             call("git reset --hard feature/branch", "/path_to/a_git_repository"),
         ])
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_update_repository_remote_mismatch(self, io_mock):
         io_mock.exists.return_value = True
         io_mock.execute.side_effect = ["git@github.com:org/repo.git", "", ""]
@@ -203,7 +192,6 @@ class TestGitLibrary:
             call("git reset --hard origin/master", "/path_to/a_git_repository"),
         ])
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_update_repository_remote_match_update(self, io_mock):
         io_mock.exists.return_value = True
         io_mock.execute.side_effect = ["git@github.com:org/repo.git", "", "", ""]
@@ -218,7 +206,6 @@ class TestGitLibrary:
             call("git reset --hard origin/master", "/path_to/a_git_repository"),
         ])
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_update_repository_directory_is_not_repository(self, io_mock):
         io_mock.exists.return_value = True
         io_mock.execute.side_effect = [
@@ -237,21 +224,18 @@ class TestGitLibrary:
             call("git reset --hard origin/master", "/path_to/a_git_repository"),
         ])
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_clone(self, io_mock):
         """Should send the right command to io lib."""
         git.clone('some/path', 'ssh://some-external-address/project.git', 'step-1')
         io_mock.execute.assert_called_once_with('git clone ssh://some-external-address/project.git -b step-1',
                                                 'some/path')
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_clone_with_default_branch(self, io_mock):
         """Should send the right command to io lib using master as default branch."""
         git.clone('some/path', 'ssh://some-external-address/project.git')
         io_mock.execute.assert_called_once_with('git clone ssh://some-external-address/project.git -b master',
                                                 'some/path')
 
-    @patch('nestor_api.lib.git.io', autospec=True)
     def test_get_branch_hash(self, io_mock):
         """Should send the right command to io lib."""
         git.get_branch_hash('ssh://some-external-address/project.git', 'master')
