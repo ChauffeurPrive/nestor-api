d6dadcaba8292f3069c13bcee7e86936fca5d5a9
Patrick Portal <patrick.portal@kapten.com>
2020-07-15 11:33:47 +0200

    Use unittest in validator lib tests

--- tests/config_validator/test_config.py
+++ tests/config_validator/test_config.py
@@ -1,11 +1,15 @@
+import os
+import unittest
+from unittest.mock import patch
+
 from validator.config.config import Configuration
 
 
-def test_config_get_target_path(monkeypatch):
-    monkeypatch.setenv("NESTOR_CONFIG_PATH", "/some/path")
-    assert Configuration.get_target_path() == "/some/path"
+class TestConfig(unittest.TestCase):
+    @patch.dict(os.environ, {"NESTOR_CONFIG_PATH": "/some/path"})
+    def test_config_get_target_path(self):
+        assert Configuration.get_target_path() == "/some/path"
 
-
-def test_config_get_validation_target(monkeypatch):
-    monkeypatch.setenv("NESTOR_VALIDATION_TARGET", "APPLICATIONS")
-    assert Configuration.get_validation_target() == "APPLICATIONS"
+    @patch.dict(os.environ, {"NESTOR_VALIDATION_TARGET": "APPLICATIONS"})
+    def test_config_get_validation_target(self):
+        assert Configuration.get_validation_target() == "APPLICATIONS"
