7d7ce4c5ff13da5802c5f74c6ce473c972c082d3
Christophe Verclytte <1611574+chris-verclytte@users.noreply.github.com>
2020-07-28 19:57:24 +0200

    Improve init_workflow concurrency handling

--- nestor_api/api/api_routes/workflow/workflow.py
+++ nestor_api/api/api_routes/workflow/workflow.py
@@ -2,7 +2,9 @@
 from http import HTTPStatus
 
 from nestor_api.adapters.git.provider import get_git_provider
+from nestor_api.config.config import Configuration
 import nestor_api.lib.config as config_lib
+import nestor_api.lib.io as io_lib
 import nestor_api.lib.workflow as workflow_lib
 from nestor_api.utils.logger import Logger
 
@@ -17,7 +19,9 @@ def init_workflow(organization, app):
     report = None
     try:
         # Retrieve project configuration
-        project_config = config_lib.get_project_config()
+        config_dir = config_lib.create_temporary_config_copy()
+        config_lib.change_environment(Configuration.get_config_default_branch(), config_dir)
+        project_config = config_lib.get_project_config(config_dir)
 
         git_provider = get_git_provider(project_config)
         report_status, report = workflow_lib.init_workflow(organization, app, git_provider)
@@ -30,6 +34,15 @@ def init_workflow(organization, app):
             message = "Workflow initialization succeeded"
         else:
             raise ValueError(f"Unexpected status: '{report_status}'")
+
+        # Clean the temporary directory
+        try:
+            io_lib.remove(config_dir)
+        # pylint: disable=broad-except
+        except Exception as err:
+            Logger.warn(
+                {"config_dir": config_dir, "err": err}, "Failed to clean temporary config dir"
+            )
 
         # HTTP Response
         Logger.info(
--- tests/api/api_routes/workflow/test_workflow.py
+++ tests/api/api_routes/workflow/test_workflow.py
@@ -7,6 +7,7 @@ from nestor_api.api.flask_app import create_app
 
 
 @patch("nestor_api.api.api_routes.workflow.workflow.Logger", autospec=True)
+@patch("nestor_api.api.api_routes.workflow.workflow.io_lib", autospec=True)
 class TestWorkflow(TestCase):
     def setUp(self):
         app = create_app()
@@ -17,7 +18,7 @@ class TestWorkflow(TestCase):
     @patch("nestor_api.api.api_routes.workflow.workflow.workflow_lib", autospec=True)
     @patch("nestor_api.api.api_routes.workflow.workflow.config_lib", autospec=True)
     def test_init_workflow(
-        self, config_mock, workflow_lib_mock, get_git_provider_mock, _logger_mock
+        self, config_mock, workflow_lib_mock, get_git_provider_mock, io_mock, _logger_mock
     ):
         """Should properly return success response containing report."""
         # Mock
@@ -45,14 +46,15 @@ class TestWorkflow(TestCase):
             },
         )
 
-        config_mock.get_project_config.assert_called_once_with()
+        config_mock.get_project_config.assert_called_once_with("fake-path")
         get_git_provider_mock.assert_called_with(fake_config)
+        io_mock.remove.assert_called_with("fake-path")
 
     @patch("nestor_api.api.api_routes.workflow.workflow.get_git_provider", autospec=True)
     @patch("nestor_api.api.api_routes.workflow.workflow.workflow_lib", autospec=True)
     @patch("nestor_api.api.api_routes.workflow.workflow.config_lib", autospec=True)
     def test_init_workflow_failing(
-        self, config_mock, workflow_lib_mock, get_git_provider_mock, _logger_mock
+        self, config_mock, workflow_lib_mock, get_git_provider_mock, io_mock, _logger_mock
     ):
         """Should properly return fail response containing report."""
         # Mock
@@ -77,17 +79,19 @@ class TestWorkflow(TestCase):
             },
         )
 
-        config_mock.get_project_config.assert_called_once_with()
+        config_mock.get_project_config.assert_called_once_with("fake-path")
         get_git_provider_mock.assert_called_with(fake_config)
+        io_mock.remove.assert_called_with("fake-path")
 
     @patch("nestor_api.api.api_routes.workflow.workflow.get_git_provider", autospec=True)
     @patch("nestor_api.api.api_routes.workflow.workflow.workflow_lib", autospec=True)
     @patch("nestor_api.api.api_routes.workflow.workflow.config_lib", autospec=True)
     def test_init_workflow_return_unexpected_status(
-        self, config_mock, workflow_lib_mock, get_git_provider_mock, _logger_mock
+        self, config_mock, workflow_lib_mock, get_git_provider_mock, io_mock, _logger_mock
     ):
         """Should properly return fail response containing error."""
         # Mock
+        config_mock.create_temporary_config_copy.return_value = "fake-path"
         fake_config = {"git": {"provider": "some-provider"}}
         config_mock.get_project_config.return_value = fake_config
         get_git_provider_mock.return_value = MagicMock(spec=AbstractGitProvider)
@@ -109,5 +113,44 @@ class TestWorkflow(TestCase):
             },
         )
 
-        config_mock.get_project_config.assert_called_once_with()
+        config_mock.get_project_config.assert_called_once_with("fake-path")
         get_git_provider_mock.assert_called_with(fake_config)
+        io_mock.remove.assert_not_called()
+
+    @patch("nestor_api.api.api_routes.workflow.workflow.get_git_provider", autospec=True)
+    @patch("nestor_api.api.api_routes.workflow.workflow.workflow_lib.init_workflow", autospec=True)
+    @patch("nestor_api.api.api_routes.workflow.workflow.config_lib", autospec=True)
+    def test_init_workflow_with_fail_during_cleaning(
+        self, config_mock, init_workflow_mock, get_git_provider_mock, io_mock, _logger_mock
+    ):
+        """Should not prevent the server to respond properly."""
+        # Mock
+        config_mock.create_temporary_config_copy.return_value = "fake-path"
+        fake_config = {"git": {"provider": "some-provider"}}
+        config_mock.get_project_config.return_value = fake_config
+        get_git_provider_mock.return_value = MagicMock(spec=AbstractGitProvider)
+        init_workflow_mock.return_value = (
+            WorkflowInitStatus.SUCCESS,
+            {"integration": {"created": (True, True), "protected": (True, True)}},
+        )
+        io_mock.remove.side_effect = Exception("Some error during cleaning")
+
+        # Tests
+        response = self.app_client.post("/api/workflow/init/my-org/my-app")
+        data = response.get_json()
+
+        # Assertions
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertEqual(
+            data,
+            {
+                "organization": "my-org",
+                "message": "Workflow initialization succeeded",
+                "app": "my-app",
+                "report": {"integration": {"created": [True, True], "protected": [True, True]}},
+            },
+        )
+
+        config_mock.get_project_config.assert_called_once_with("fake-path")
+        get_git_provider_mock.assert_called_with(fake_config)
+        io_mock.remove.assert_called_with("fake-path")
