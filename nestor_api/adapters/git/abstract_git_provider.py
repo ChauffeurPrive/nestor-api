77e191c9a8a8f2a5df5feeffc9fb64ee2aa12d50
Christophe Verclytte <1611574+chris-verclytte@users.noreply.github.com>
2020-08-03 15:28:42 +0200

    Update github provider error handling

--- nestor_api/adapters/git/abstract_git_provider.py
+++ nestor_api/adapters/git/abstract_git_provider.py
@@ -1,6 +1,7 @@
 """Abstract adapter for git provider."""
 
 from abc import ABC, abstractmethod
+from enum import Enum
 
 
 class AbstractGitProvider(ABC):
@@ -12,20 +13,38 @@ class AbstractGitProvider(ABC):
         raise NotImplementedError()
 
     @abstractmethod
-    def get_branch(self, organization: str, app_name: str, branch_name: str):
+    def get_branch(self, organization: str, repository_name: str, branch_name: str):
         """Get branch information."""
         raise NotImplementedError()
 
     @abstractmethod
-    def create_branch(self, organization: str, app_name: str, branch_name: str, ref: str):
+    def create_branch(self, organization: str, repository_name: str, branch_name: str, ref: str):
         """Create a branch."""
         raise NotImplementedError()
 
     @abstractmethod
-    def protect_branch(self, organization: str, app_name: str, branch_name: str, user_login: str):
+    def protect_branch(
+        self, organization: str, repository_name: str, branch_name: str, user_login: str
+    ):
         """Protect a branch."""
         raise NotImplementedError()
 
 
+class GitResource(Enum):
+    """Enum for Git resource."""
+
+    BRANCH = "BRANCH"
+    REPOSITORY = "REPOSITORY"
+    USER = "USER"
+
+
 class GitProviderError(Exception):
     """Base error for git provider."""
+
+
+class GitResourceNotFoundError(GitProviderError):
+    """The resource has not been found."""
+
+    def __init__(self, resource: GitResource):
+        super().__init__()
+        self.resource = resource
--- nestor_api/adapters/git/github_git_provider.py
+++ nestor_api/adapters/git/github_git_provider.py
@@ -1,11 +1,16 @@
 """Git provider adapter for Github."""
 
 from http import HTTPStatus
-from typing import Optional, Union
+from typing import Union
 
-from github import AuthenticatedUser, Branch, Github, GithubException, NamedUser
+from github import AuthenticatedUser, Branch, Github, GithubException, NamedUser, Repository
 
-from nestor_api.adapters.git.abstract_git_provider import AbstractGitProvider, GitProviderError
+from nestor_api.adapters.git.abstract_git_provider import (
+    AbstractGitProvider,
+    GitProviderError,
+    GitResource,
+    GitResourceNotFoundError,
+)
 from nestor_api.config.git import GitConfiguration
 
 
@@ -24,40 +29,44 @@ class GitHubGitProvider(AbstractGitProvider):
         except GithubException as err:
             raise _format_error(err)
 
-    def get_branch(
-        self, organization: str, app_name: str, branch_name: str
-    ) -> Optional[Branch.Branch]:
-        """Get branch information."""
+    def _get_repository(self, organization: str, repository_name: str) -> Repository.Repository:
+        """Get repository information."""
         try:
-            repository = self.__client.get_repo(f"{organization}/{app_name}")
+            return self.__client.get_repo(f"{organization}/{repository_name}")
+        except GithubException as err:
+            if err.status == HTTPStatus.NOT_FOUND:
+                raise GitResourceNotFoundError(GitResource.REPOSITORY)
+            raise _format_error(err)
+
+    def get_branch(
+        self, organization: str, repository_name: str, branch_name: str
+    ) -> Branch.Branch:
+        """Get branch information."""
+        repository = self._get_repository(organization, repository_name)
+        try:
             return repository.get_branch(branch=branch_name)
         except GithubException as err:
             if err.status == HTTPStatus.NOT_FOUND:
-                return None
+                raise GitResourceNotFoundError(GitResource.BRANCH)
             raise _format_error(err)
 
     def create_branch(
-        self, organization: str, app_name: str, branch_name: str, ref: str
+        self, organization: str, repository_name: str, branch_name: str, ref: str
     ) -> Branch.Branch:
         """Create a branch."""
+        repository = self._get_repository(organization, repository_name)
         try:
-            repository = self.__client.get_repo(f"{organization}/{app_name}")
             repository.create_git_ref(f"refs/heads/{branch_name}", ref)
-            branch = self.get_branch(organization, app_name, branch_name)
-            if branch is None:
-                raise GitProviderError("Could not retrieve branch after ref creation")
-            return branch
+            return self.get_branch(organization, repository_name, branch_name)
         except GithubException as err:
             raise _format_error(err)
 
     def protect_branch(
-        self, organization: str, app_name: str, branch_name: str, user_login: str
+        self, organization: str, repository_name: str, branch_name: str, user_login: str
     ) -> None:
         """Protect a branch."""
         try:
-            branch = self.get_branch(organization, app_name, branch_name)
-            if branch is None:
-                raise GitProviderError("Branch not found")
+            branch = self.get_branch(organization, repository_name, branch_name)
             branch.edit_protection(enforce_admins=False, user_push_restrictions=[user_login])
         except GithubException as err:
             raise _format_error(err)
