dbb85d4fd77232721b0d13bd06837135ebe23fba
Guillaume Benard <guillaume@benard.io>
2020-08-06 18:14:25 +0200

    Write list comparison utility

--- nestor_api/utils/list.py
+++ nestor_api/utils/list.py
@@ -3,6 +3,48 @@
 from typing import Any, Callable, Optional
 
 
+def compute_diff(
+    list_1: list,
+    list_2: list,
+    key=lambda x: x,
+    comparator=lambda v1, v2: None if v1 == v2 else {"old": v1, "new": v2},
+) -> dict:
+    """Compute the differences between two lists. It assumes `list_1` is the previous state
+    and `list_2` the new one when comparing."""
+    diff: dict = {"added": [], "modified": [], "removed": []}
+
+    list_1 = list_1.copy()
+    list_2 = list_2.copy()
+    list_1.sort(key=key)
+    list_2.sort(key=key)
+
+    idx_1 = 0
+    idx_2 = 0
+    while idx_1 < len(list_1) and idx_2 < len(list_2):
+        key_1 = key(list_1[idx_1])
+        key_2 = key(list_2[idx_2])
+        if key_1 < key_2:
+            diff["removed"].append(list_1[idx_1])
+            idx_1 += 1
+        elif key_1 > key_2:
+            diff["added"].append(list_2[idx_2])
+            idx_2 += 1
+        else:
+            comparison = comparator(list_1[idx_1], list_2[idx_2])
+            if comparison is not None:
+                diff["modified"].append(comparison)
+            idx_1 += 1
+            idx_2 += 1
+
+    if idx_1 < len(list_1):
+        diff["removed"].extend(list_1[idx_1:])
+
+    if idx_2 < len(list_2):
+        diff["added"].extend(list_2[idx_2:])
+
+    return diff
+
+
 def flatten(double_list: list) -> list:
     """Flatten a list of list into a simple list.
 
--- tests/utils/test_list.py
+++ tests/utils/test_list.py
@@ -4,6 +4,95 @@ import nestor_api.utils.list as list_utils
 
 
 class TestListUtil(TestCase):
+    def test_compute_diff_when_both_empty(self):
+        """Should return empty report."""
+        list_1 = []
+        list_2 = []
+
+        result = list_utils.compute_diff(list_1, list_2)
+
+        self.assertEqual(list_1, [])
+        self.assertEqual(list_2, [])
+        self.assertEqual(result, {"added": [], "modified": [], "removed": []})
+
+    def test_compute_diff_when_first_empty(self):
+        """Should consider all members of list_2 as "added" and sort them."""
+        list_1 = []
+        list_2 = [3, 2, 1]
+
+        result = list_utils.compute_diff(list_1, list_2)
+
+        self.assertEqual(list_1, [])
+        self.assertEqual(list_2, [3, 2, 1])
+        self.assertEqual(result, {"added": [1, 2, 3], "modified": [], "removed": []})
+
+    def test_compute_diff_when_second_empty(self):
+        """Should consider all members of list_1 as "removed" and sort them."""
+        list_1 = [3, 2, 1]
+        list_2 = []
+
+        result = list_utils.compute_diff(list_1, list_2)
+
+        self.assertEqual(list_1, [3, 2, 1])
+        self.assertEqual(list_2, [])
+        self.assertEqual(result, {"added": [], "modified": [], "removed": [1, 2, 3]})
+
+    def test_compute_diff_with_additions_and_deletions(self):
+        """Should correctly find the added and removed items."""
+        list_1 = [1, 3, 5, 6]
+        list_2 = [2, 3, 4, 6, 7]
+
+        result = list_utils.compute_diff(list_1, list_2)
+
+        self.assertEqual(list_1, [1, 3, 5, 6])
+        self.assertEqual(list_2, [2, 3, 4, 6, 7])
+        self.assertEqual(result, {"added": [2, 4, 7], "modified": [], "removed": [1, 5]})
+
+    def test_compute_diff_with_custom_key(self):
+        """Should correctly match the modified values."""
+        list_1 = ["a", "b", "c"]
+        list_2 = ["A", "B", "C"]
+        key_getter = lambda x: x.lower()  # The keys will match but not the values
+
+        result = list_utils.compute_diff(list_1, list_2, key=key_getter)
+
+        self.assertEqual(list_1, ["a", "b", "c"])
+        self.assertEqual(list_2, ["A", "B", "C"])
+        self.assertEqual(
+            result,
+            {
+                "added": [],
+                "modified": [
+                    {"old": "a", "new": "A"},
+                    {"old": "b", "new": "B"},
+                    {"old": "c", "new": "C"},
+                ],
+                "removed": [],
+            },
+        )
+
+    def test_compute_diff_with_complex_values(self):
+        """Should correctly use the provided comparator."""
+        list_1 = [{"key": "a", "value": 1}, {"key": "b", "value": 1}, {"key": "c", "value": 1}]
+        list_2 = [{"key": "b", "value": 1}, {"key": "c", "value": 2}, {"key": "d", "value": 2}]
+        key_getter = lambda x: x["key"]
+        comparator = (
+            lambda v1, v2: None
+            if v1["value"] == v2["value"]
+            else {v1["key"]: {"old": v1["value"], "new": v2["value"]}}
+        )
+
+        result = list_utils.compute_diff(list_1, list_2, key=key_getter, comparator=comparator)
+
+        self.assertEqual(
+            result,
+            {
+                "added": [{"key": "d", "value": 2}],
+                "modified": [{"c": {"old": 1, "new": 2}}],
+                "removed": [{"key": "a", "value": 1}],
+            },
+        )
+
     def test_flatten(self):
         """Should correctly flatten the given list."""
         double_list = [[1, 2], [3, 4], [5, 6]]
